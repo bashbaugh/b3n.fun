684b303ae86fe843e270c51302b744db80e686b4
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2021-09-11 17:36:52 -0600

    rm styles folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
+import '../public/globals.css'
 import type { AppProps } from 'next/app'
 import { Router } from 'next/router'
 import NProgress from 'nprogress'
-import '../styles/nprogress.css'
+import '../public/nprogress.css'
 
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', () => NProgress.done())
