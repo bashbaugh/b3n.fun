9998488035144ada7bf0addf36d1edb8c62e0c70
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2021-11-08 19:20:10 -0700

    simple analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script async defer src="https://numbers.b3n.fun/latest.js"></script>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
